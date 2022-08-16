7de758e95f1d2748cc1ec2a3418c554662212756
kyle5328 <laukayan201099@yahoo.com.hk>
2022-08-16 17:55:12 +0800

    艾琳套Kinect偵測 投降動作捕捉（完成）

---
+++ Alive/Assets/K2Examples/KinectScripts/Samples/SimpleVisualGestureListener.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleVisualGestureListener : MonoBehaviour, VisualGestureListenerInterface
+{
+    public bool GestureCompleted(long userId, int userIndex, string gesture, float confidence)
+    {
+        if (gesture == "Freeze") //如果举起右手姿态完成
+        {
+            print("我投降...");
+            Debug.Log("我投降...");
+        }
+        return true;
+        //throw new System.NotImplementedException();
+    }
+    //检测动作姿态过程
+    public void GestureInProgress(long userId, int userIndex, string gesture, float progress)
+    {
+
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+}
